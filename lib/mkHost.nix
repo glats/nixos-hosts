ccfb031179d31920acc1f3e25981cffa301815d7
glats <glats@users.noreply.github.com>
2026-04-17 21:35:35 -0400

    fix: mkHost.nix should return a set with mkHost attribute

--- lib/mkHost.nix
+++ lib/mkHost.nix
@@ -1,28 +1,32 @@
 { inputs, ... }:
 
-{ hostname
-, system ? "x86_64-linux"
-, extraModules ? []
-}:
+let
+  mkHost = { hostname
+  , system ? "x86_64-linux"
+  , extraModules ? []
+  }:
+  inputs.nixpkgs.lib.nixosSystem {
+    inherit system;
+    specialArgs = { inherit inputs; };
+    modules = [
+      # Host-specific configuration
+      ../hosts/${hostname}
 
-inputs.nixpkgs.lib.nixosSystem {
-  inherit system;
-  specialArgs = { inherit inputs; };
-  modules = [
-    # Host-specific configuration
-    ../hosts/${hostname}
+      # Sops-nix for secrets management
+      inputs.sops-nix.nixosModules.sops
 
-    # Sops-nix for secrets management
-    inputs.sops-nix.nixosModules.sops
+      # Home Manager integrated with NixOS
+      inputs.home-manager.nixosModules.home-manager
 
-    # Home Manager integrated with NixOS
-    inputs.home-manager.nixosModules.home-manager
-
-    # Overlays for custom packages
-    {
-      nixpkgs.overlays = [
-        (import ../modules/core/overlays.nix { self = inputs.self; })
-      ];
-    }
-  ] ++ extraModules;
+      # Overlays for custom packages
+      {
+        nixpkgs.overlays = [
+          (import ../modules/core/overlays.nix { self = inputs.self; })
+        ];
+      }
+    ] ++ extraModules;
+  };
+in
+{
+  inherit mkHost;
 }
