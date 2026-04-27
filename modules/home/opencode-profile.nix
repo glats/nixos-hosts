41823ec95781069147cddff57c735d60c3fa04c3
Juan Cuzmar <jcuzmar@protonmail.com>
2026-04-26 20:04:38 -0400

    work

--- modules/home/opencode-profile.nix
+++ modules/home/opencode-profile.nix
@@ -6,9 +6,6 @@
     runtime = "stable";
     legacyFallback = false;
 
-    # Model override - uses Fireworks by default (change to deepinfra or remove to customize)
-    agentOverrides.sdd-orchestrator.model = "fireworks/accounts/fireworks/models/kimi-k2p6";
-
     plugins = {
       backgroundAgents.enable = true;
       engram.enable = true;
