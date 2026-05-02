ff7846568a0b3e8d6d6ec2dd79e9523ada97b36a
glats <glats@users.noreply.github.com>
2026-05-02 00:19:53 -0400

    stuff

--- modules/home/opencode.nix
+++ modules/home/opencode.nix
@@ -59,7 +59,7 @@ let
           plugin = [ "opencode-model-fallback-chain" ];
           experimental = {
             modelFallbackChain = {
-              timeoutMs = 60000;
+              timeoutMs = 3600000;
               chains = [
                 [ "nvidia/deepseek-ai/deepseek-v4-pro" "nvidia/z-ai/glm-5.1" ]
                 [ "nvidia/deepseek-ai/deepseek-v4-flash" "nvidia/minimaxai/minimax-m2.7" ]
