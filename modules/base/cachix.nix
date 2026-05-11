8b6b528423f6a76dd0bab05cadc13443fb01bba1
glats <glats@users.noreply.github.com>
2026-05-10 20:45:53 -0400

    fix(cachix): correct flox-cache-public trusted key format

--- modules/base/cachix.nix
+++ modules/base/cachix.nix
@@ -17,7 +17,7 @@
       "nix-community.cachix.org-1:7Nw0m1eeP3Gg3RhbC8Vy/Z4GqW2ZJYX9F8Nc8eeeCJ8="
       "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
       "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-      "flox-cache-public"
+      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
       "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
     ];
   };
