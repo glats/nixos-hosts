18b030dcf1d5b570071b1682835f91932e5c663a
Juan Cuzmar <jcuzmar@protonmail.com>
2026-06-14 00:36:26 -0400

    new providers

--- shared/opencode/providers-base.nix
+++ shared/opencode/providers-base.nix
@@ -148,11 +148,11 @@ let
         gentle-orchestrator = "nvidia/moonshotai/kimi-k2.6";
         sdd-init = "opencode/deepseek-v4-flash-free";
         sdd-explore = "opencode-go/qwen3.7-plus";
-        sdd-propose = "opencode/mimo-v2.5-free";
+        sdd-propose = "nvidia/moonshotai/kimi-k2.6";
         sdd-spec = "opencode-go/qwen3.7-plus";
         sdd-design = "opencode/nemotron-3-ultra-free";
-        sdd-tasks = "opencode/mimo-v2.5-free";
-        sdd-apply = "opencode-go/minimax-m3";
+        sdd-tasks = "nvidia/moonshotai/kimi-k2.6";
+        sdd-apply = "nvidia/minimaxai/minimax-m3";
         sdd-verify = "opencode/nemotron-3-ultra-free";
         sdd-archive = "opencode/deepseek-v4-flash-free";
         sdd-onboard = "opencode/deepseek-v4-flash-free";
