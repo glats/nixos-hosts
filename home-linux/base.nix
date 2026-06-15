f914f12d8e34d600ff0c461f095995e43e7f7b76
Juan Cuzmar <jcuzmar@protonmail.com>
2026-06-15 14:39:54 -0400

    work

--- home-linux/base.nix
+++ home-linux/base.nix
@@ -5,4 +5,8 @@
   home.homeDirectory = "/home/${username}";
   home.stateVersion = "25.05";
   home.enableNixpkgsReleaseCheck = false;
+
+  # Explicit opt-in silencing eval warning.
+  # Default flips true -> false for stateVersion >= 26.05.
+  xdg.userDirs.setSessionVariables = true;
 }
