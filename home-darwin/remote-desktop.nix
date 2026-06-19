4e41f06fcaaed8b5753c11d871cdf44235101bdb
Juan Cuzmar <jcuzmar@protonmail.com>
2026-06-18 22:55:10 -0400

    work

--- home-darwin/remote-desktop.nix
+++ home-darwin/remote-desktop.nix
@@ -25,7 +25,9 @@ let
               echo "       Instalar con: brew install --cask tigervnc" >&2
               exit 1
             fi
-            exec "$VNC_BIN" "${host}${if port != "" then ":${port}" else ""}"
+            exec "$VNC_BIN" -FullScreen -FullscreenSystemKeys -RemoteResize "${host}${
+              if port != "" then ":${port}" else ""
+            }"
           ''
         else
           "${pkgs.freerdp}/bin/sdl-freerdp /v:${host} /u:${username} /p: /cert:ignore /sound:sys:mac /clipboard /w:1920 /h:1080 /smart-sizing /gfx:progressive /bpp:32 /kbd:layout:0x0000040A,lang:0x040A";
