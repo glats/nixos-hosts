f270bb96ca401695cf46ac33a0eee6305945e958
Juan Cuzmar <jcuzmar@protonmail.com>
2026-06-25 00:06:29 -0400

    work

--- modules/base/logind.nix
+++ modules/base/logind.nix
@@ -6,5 +6,9 @@
     HandleSuspendKey = "suspend";
     HandleHibernateKey = "hibernate";
     HandleLidSwitch = "ignore";
+    # Explicitly disable logind-level idle locking.
+    # Without this, systemd-logind can auto-lock sessions via IdleAction,
+    # which would lock the screen even when hypridle is stopped.
+    IdleAction = "ignore";
   };
 }
