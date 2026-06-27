c6e7750f30d2f41ff5dd83a7843045bbc0b89d84
Juan Cuzmar <jcuzmar@protonmail.com>
2026-06-26 23:04:45 -0400

    work

--- home-linux/btop-settings.nix
+++ home-linux/btop-settings.nix
@@ -8,8 +8,9 @@
 # Imported by `hosts/t14/home/omarchy.nix`. The `glats` theme
 # file is written by the shared `home-linux/btop-theme.nix` module
 # (also imported by `omarchy.nix`).
-{ lib
-, ...
+{
+  lib,
+  ...
 }:
 {
   programs.btop.settings = {
