58305443b6d8435511748e66664f6a68879c0559
Juan Cuzmar <jcuzmar@protonmail.com>
2026-08-21 22:31:15 -0400

    default provider

--- hosts/t14/home/default.nix
+++ hosts/t14/home/default.nix
@@ -23,6 +23,6 @@ builtins.filter (m: !builtins.elem m excluded) base
 ++ [
   ./omarchy.nix
   ../../../linux/home/remote-desktop.nix
-  ({ home.opencode.activeProviderName = "openai-medium"; })
+  ({ home.opencode.activeProviderName = "openai-full"; })
   inputs.hyprdynamicmonitors.homeManagerModules.default
 ]
