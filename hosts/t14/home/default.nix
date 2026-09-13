2224cc22616146cb91a4c91c941896f8bbb8f2ad
Juan Cuzmar <jcuzmar@protonmail.com>
2026-09-12 22:26:41 -0300

    work

--- hosts/t14/home/default.nix
+++ hosts/t14/home/default.nix
@@ -23,6 +23,6 @@ builtins.filter (m: !builtins.elem m excluded) base
 ++ [
   ./omarchy.nix
   ../../../linux/home/remote-desktop.nix
-  ({ home.opencode.activeProviderName = "opencode-free"; })
+  ({ home.opencode.activeProviderName = "openai-medium"; })
   inputs.hyprdynamicmonitors.homeManagerModules.default
 ]
