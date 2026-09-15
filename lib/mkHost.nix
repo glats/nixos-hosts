e1c79ad32510c419b596e5df7893c5eb26229d32
glats <glats@users.noreply.github.com>
2026-09-14 23:52:45 -0300

    feat(t14): add isolated quattro package boundary

--- lib/mkHost.nix
+++ lib/mkHost.nix
@@ -6,34 +6,45 @@ let
     , system ? "x86_64-linux"
     , username ? "glats"
     , extraModules ? [ ]
+    , extraOverlays ? [ ]
+    , nixpkgsInput ? inputs.nixpkgs
+    , homeManagerInput ? inputs.home-manager
     ,
     }:
-    inputs.nixpkgs.lib.nixosSystem {
+    let
+      hostInputs = inputs // {
+        nixpkgs = nixpkgsInput;
+        home-manager = homeManagerInput;
+      };
+    in
+    nixpkgsInput.lib.nixosSystem {
       inherit system;
       specialArgs = {
-        inherit inputs self username;
+        inherit self username;
+        inputs = hostInputs;
       };
       modules = [
         # Host-specific configuration
         ../hosts/${hostname}
 
         # Sops-nix for secrets management
-        inputs.sops-nix.nixosModules.sops
+        hostInputs.sops-nix.nixosModules.sops
 
         # Home Manager integrated with NixOS
-        inputs.home-manager.nixosModules.home-manager
+        homeManagerInput.nixosModules.home-manager
 
         # Overlays for custom packages
         {
           nixpkgs.overlays = [
-            (import ../overlays/linux.nix { inherit self inputs; })
-          ];
+            (import ../overlays/linux.nix { inherit self; inputs = hostInputs; })
+          ] ++ extraOverlays;
         }
 
         # Pass inputs to home-manager for module access
         {
           home-manager.extraSpecialArgs = {
-            inherit inputs username;
+            inherit username;
+            inputs = hostInputs;
           };
         }
       ]
